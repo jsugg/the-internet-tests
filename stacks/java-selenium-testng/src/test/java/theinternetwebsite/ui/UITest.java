2e24ed0ceeb1af9ea276c4cfe3489c891ec85833
Juan Sugg <juanpedrosugg@gmail.com>
2026-07-10 04:00:26 -0300

    fix(java): correct control character cleanup

--- stacks/java-selenium-testng/src/test/java/theinternetwebsite/ui/UITest.java
+++ stacks/java-selenium-testng/src/test/java/theinternetwebsite/ui/UITest.java
@@ -182,7 +182,7 @@ public class UITest {
         // strips off all non-ASCII characters
         text = text.replaceAll("[^\\x00-\\x7F]", "");
         // erases all the ASCII control characters
-        text = text.replaceAll("\\p{Cntrl}&&[^\r\n\t]", "");
+        text = text.replaceAll("[\\p{Cntrl}&&[^\\r\\n\\t]]", "");
         // removes non-printable characters from Unicode
         text = text.replaceAll("\\p{C}", "");
         return text.trim();
